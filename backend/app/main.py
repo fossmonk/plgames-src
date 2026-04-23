4f3d39c80a8b7dc57bfebfb81647cf50a08f2fe0
Anand Mahadevan SS <mahadevananand76@gmail.com>
2026-04-23 17:22:09 +0530

    Update CORS URL
    Signed-off-by: Anand Mahadevan SS <mahadevananand76@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://pinklungigames.com",
+    "https://www.pinklungigames.com",
 ]
 
 app.add_middleware(
