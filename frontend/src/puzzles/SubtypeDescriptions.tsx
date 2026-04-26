d8e6d3334dcac54dd82caba7d46a7eda59bc10cd
Anand Mahadevan SS <mahadevananand76@gmail.com>
2026-04-26 09:58:03 +0530

    Revise descriptions for connections
    Signed-off-by: Anand Mahadevan SS <mahadevananand76@gmail.com>

--- frontend/src/puzzles/SubtypeDescriptions.tsx
+++ frontend/src/puzzles/SubtypeDescriptions.tsx
@@ -2,18 +2,13 @@ const SUBTYPE_DESC_MAP: Record<string, string> = {
   connections: `
   If you're familiar with NYT Connections, you can skip the rest 😎
 
-  For the others, your goal is to find 4 groups of 4 such that the terms in each group 
-  belong to a specific category. When you successfully identify a group, its category 
-  is revealed along with a color-coded difficulty level: categories are rated 
-  yellow, green, blue and purple, with yellow being the most straightforward and 
-  purple being the most difficult. The categories may involve wordplay such as 
-  palindromes or homophones, increasing the difficulty.
-
-  CONNECT AWAY!
+  For the others, Find four groups of four words that share a common 
+  theme by tapping on the tiles and clicking on submit. Once identified, 
+  each category is revealed by a color-coded difficulty level. Can you solve all four?
   `,
   mcq: `
   There will be 4 options for each question in quiz. You can select one and submit. You can't
-  change your choice once selected. Your score will be shown after you have answered all
+  change your choice once submitted. Your score will be shown after you have answered all
   the questions.
   `
 };
